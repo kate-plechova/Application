c92ed148c64450302cd2d02ea450cadd1b064e2f
DanielOlivo <vit.masterov@gmail.com>
2026-01-08 19:57:06 +0200

    api - preparing header

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_URL,
+        prepareHeaders: (headers) => {
+            const token = localStorage.getItem('token')
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`)
+            }
+            return headers
+        }
+    }),
     endpoints: () => ({})
 })

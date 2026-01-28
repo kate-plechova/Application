e1c3dbb05c708cb8a4e35f248cd403190b4ac1a7
DanielOlivo <vit.masterov@gmail.com>
2026-01-28 19:49:16 +0200

    stats.api

---
+++ client/src/stats/stats.api.ts
@@ -0,0 +1,26 @@
+import { api } from "../api";
+import { StatisticsDto } from "./stats.dto";
+import { plainToInstance } from 'class-transformer'
+
+export const statsApi = api.injectEndpoints({
+    endpoints: builder => ({
+
+        getStats: builder.query<StatisticsDto, void>({
+            queryFn: async () => {
+                try {
+                    const response = await fetch("/statistics.json");
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    const json = await response.json();
+                    const data = plainToInstance(StatisticsDto, json);
+                    return { data };
+                }
+                catch (error) {
+                    return { error: { status: 'CUSTOM_ERROR', error: error instanceof Error ? error.message : 'An unknown error occurred' } };
+                }
+            }
+        })
+
+    })
+})

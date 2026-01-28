bebed1cad11908f128f01bd9e15f4538bd1cb993
DanielOlivo <vit.masterov@gmail.com>
2026-01-28 22:13:38 +0200

    lang stats both table and pie chart

--- client/src/stats/components/LanguageStats.tsx
+++ client/src/stats/components/LanguageStats.tsx
@@ -1,34 +1,73 @@
-import type { FC } from "react"
+import { useMemo, type FC } from "react"
 import type { LanguageStatDto } from "../stats.dto"
+import { PieChart } from "@mui/x-charts"
 
 export interface LanguageStatsProps {
     data: LanguageStatDto[]
 }
 
 export const LangaugeStats: FC<LanguageStatsProps> = ({data}) => {
+
+    const chartData = useMemo(() => {
+        
+        const major = data.slice(0, 10)
+        const minorPercentage = data.slice(10).reduce((acc, item) => acc + item.total_books, 0)
+
+        return [
+            {
+                data: [
+                    ...major.map((item, i) => ({
+                        id: i,
+                        value: item.total_books,
+                        label: item.language_name
+                    })).slice(0, 10),
+                    {
+                        id: major.length,
+                        value: minorPercentage,
+                        label: "Other"
+                    }
+                ]
+            }
+        ]
+    }, [data])
+
     return (
         <div>
             <h2>Language stats</h2>
-            <div className="overflow-y-auto h-96">
-                <table className="table table-xs table-pin-rows">
-                    <thead>
-                        <th>Name</th>
-                        <th>Total books</th>
-                        <th>Translated</th>
-                        <th>Translation Percentage</th>
-                    </thead>
+            <div className="flex flex-row justify-between items-center">
 
-                    <tbody>
-                        {data.map(item => (
+                <div className="overflow-y-auto h-96">
+                    <table className="table table-pin-rows">
+                        <thead>
                             <tr>
-                                <td>{item.language_name}</td>
-                                <td>{item.total_books}</td>
-                                <td>{item.translated_books}</td>
-                                <td>{item.translation_percentage}</td>
+                                <th>Name</th>
+                                <th>Total books</th>
+                                <th>Translated</th>
+                                <th>Translation Percentage</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+
+                        <tbody>
+                            {data.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item.language_name}</td>
+                                    <td>{item.total_books}</td>
+                                    <td>{item.translated_books}</td>
+                                    <td>{item.translation_percentage}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="flex justify-center items-center">
+                    <PieChart
+                        series={chartData}
+                        width={200}
+                        height={200}
+                    />
+                </div>
+                
             </div>
         </div>
     )

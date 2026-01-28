24e554ad9c45f2744da968f94a4c8350e97ea3ac
DanielOlivo <vit.masterov@gmail.com>
2026-01-28 18:42:16 +0200

    react-router added to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,19 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { StatPage } from './stats/StatPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path="/stats" element={<StatPage />} />
+        </Routes>
+        {/* <App /> */}
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )

8a98e9b4f749f4167574cd839e7fcfb5b4e5d3cb
DanielOlivo <vit.masterov@gmail.com>
2026-01-29 20:53:59 +0200

    routes were moved to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,33 @@
 // import './App.css'
 import type { FC } from 'react'
 import { BookPage } from './books/components/BookPage/BookPage'
-import { Header } from './user/components/Header/Header'
+// import { Header } from './user/components/Header/Header'
+import { Route, Routes } from 'react-router'
+import { StatPage } from './stats/StatPage'
+import { MainLayout } from './common/MainLayout'
+import { Bookmarks } from './user/components/Bookmarks'
 
 export const App: FC = () => {
 
   return (
-    <div 
-      className='w-full min-h-full m-0 p-0 flex flex-col overflow-hidden'
-      data-theme="light"
-    >
-      <Header />
-      <BookPage />
-    </div>
+    <Routes>
+      {/* <Route path='/' element={<App />} />
+      <Route path="/stats" element={<StatPage />} /> */}
+
+      <Route path="/" element={<MainLayout />}>
+        {/* <Route index element={<div>DUDE</div>} /> */}
+        <Route index element={<BookPage />} />
+        <Route path="stats" element={<StatPage />} />
+        <Route path="bookmarks" element={<Bookmarks />} />
+      </Route>
+    </Routes>
+    // <div 
+    //   className='w-full min-h-full m-0 p-0 flex flex-col overflow-hidden'
+    //   data-theme="light"
+    // >
+    //   <Header />
+    //   <BookPage />
+    // </div>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,28 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import { StatPage } from './stats/StatPage.tsx'
-import { MainLayout } from './common/MainLayout.tsx'
-import { BookPage } from './books/components/BookPage/BookPage.tsx'
-import { Bookmarks } from './user/components/Bookmarks.tsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path="/stats" element={<StatPage />} />
-
-          <Route path="layout" element={<MainLayout />}>
-            {/* <Route index element={<div>DUDE</div>} /> */}
-            <Route index element={<BookPage />} />
-            <Route path="stats" element={<StatPage />} />
-            <Route path="bookmarks" element={<Bookmarks />} />
-          </Route>
-        </Routes>
-        {/* <App /> */}
+        <App />
       </BrowserRouter>
     </Provider>
   </StrictMode>,

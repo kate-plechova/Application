340598725c431b1a05cb1da168d15f1a11756904
DanielOlivo <vit.masterov@gmail.com>
2026-01-29 19:14:59 +0200

    main layout

--- client/src/common/MainLayout.tsx
+++ client/src/common/MainLayout.tsx
@@ -5,12 +5,14 @@ import { Header } from "../user/components/Header/Header";
 export const MainLayout: FC = () => {
     return (
         <div 
-            className="w-full h-full flex flex-col justify-between items-stretch"
+            className="w-full h-full p-0 m-0 flex flex-col justify-center items-stretch"
             data-theme="light"
         >
             <Header />
-            <div className="grow w-full h-full">
-                <Outlet />
+            <div className="grow w-full h-screen">
+                <div className="w-full h-full overflow-y-auto pt-10">
+                    <Outlet />
+                </div>
             </div>
         </div>
     )
